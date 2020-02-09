71244e71fd9255dffbcd53d0d3d1080769c33c3c
wl8442 <wl8442@rit.edu>
2020-02-09 07:27:14 -0500

    added select friend and group

--- app/src/main/java/com/example/spont/PingFriendOrGroup.java
+++ app/src/main/java/com/example/spont/PingFriendOrGroup.java
@@ -2,7 +2,9 @@ package com.example.spont;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PingFriendOrGroup extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class PingFriendOrGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ping_friend_or_group);
+    }
+
+    public void goToFriend(View view) {
+        startActivity(new Intent(PingFriendOrGroup.this, SelectFriend.class));
+    }
+
+    public void goToGroup(View view) {
+        startActivity(new Intent(PingFriendOrGroup.this, SelectGroup.class));
     }
 }
