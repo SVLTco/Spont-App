123720af00ee8d61670f84e807b038415484503b
wl8442 <wl8442@rit.edu>
2020-02-09 10:31:18 -0500

    logo added to loading screen but it won't load

--- app/src/main/java/com/example/spont/LoadingScreen.java
+++ app/src/main/java/com/example/spont/LoadingScreen.java
@@ -1,8 +1,12 @@
 package com.example.spont;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 
 
 public class LoadingScreen extends AppCompatActivity {
@@ -11,8 +15,21 @@ public class LoadingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+        ImageView loadImage = (ImageView) findViewById(R.id.logo);
+        int imageSource = getResources().getIdentifier("@drawable/svlt", null,
+                this.getPackageName());
+        loadImage.setImageResource(imageSource);
 
         //delay
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+        startActivity(new Intent(LoadingScreen.this, Login.class));
 
 
     }
