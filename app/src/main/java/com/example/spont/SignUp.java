dd1cdf9ec49728768580e7979778005dde9c5c53
mark-a-maluenda <themarkmaluenda@gmail.com>
2020-02-09 05:23:37 -0600

    Extraction of User Input

--- app/src/main/java/com/example/spont/SignUp.java
+++ app/src/main/java/com/example/spont/SignUp.java
@@ -5,16 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    EditText confirmPassword;
+    Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        /* Declaring Objects */
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        confirmPassword = (EditText)findViewById(R.id.confirmPassword);
+        signInButton = (Button)findViewById(R.id.signInButton);
+
+        /* Submit Button */
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                System.out.println(user);
+                System.out.println(pass);
+            }
+        });
     }
 
     public void goToMap(View view) {
         startActivity(new Intent(SignUp.this, Map.class));
     }
+
+
 }
