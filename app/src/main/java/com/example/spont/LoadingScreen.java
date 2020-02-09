779bd5d81feb4707149d1e5ed477e73681c325ae
wl8442 <wl8442@rit.edu>
2020-02-09 09:19:55 -0500

    add loading screen

---
+++ app/src/main/java/com/example/spont/LoadingScreen.java
@@ -0,0 +1,20 @@
+package com.example.spont;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        //delay
+
+
+    }
+
+}
